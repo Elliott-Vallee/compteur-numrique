a652ede71975d2276e2a0aa8dfcd8b35bd00a42d
Elliott-Vallee <116816433+Elliott-Vallee@users.noreply.github.com>
2022-10-27 09:44:15 -0400

    Mettre à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,12 @@ input.onButtonPressed(Button.A, function () {
     Nombre += 1
     basic.showNumber(Nombre)
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.AB, function () {
     Nombre = 0
+    basic.showNumber(Nombre)
+})
+input.onButtonPressed(Button.B, function () {
+    Nombre += -1
     basic.showNumber(Nombre)
 })
 basic.forever(function () {
