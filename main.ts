c8a1b4d56cd7ac9412888e36354dca0504cd8a28
Elliott-Vallee <116816433+Elliott-Vallee@users.noreply.github.com>
2022-10-27 09:42:12 -0400

    Fichiers initiaux du projet MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let Nombre = 0
+input.onButtonPressed(Button.A, function () {
+    Nombre += 1
+    basic.showNumber(Nombre)
+})
+input.onButtonPressed(Button.B, function () {
+    Nombre = 0
+    basic.showNumber(Nombre)
+})
+basic.forever(function () {
+    if (10 == Nombre) {
+        basic.showString("MAX")
+    }
+})
